af77d9ddf1e581cfe4406826a5fa0db280f1229f
shubh-verma <shubhvcs@gmail.com>
2024-02-07 02:46:56 +0530

    [Add] passing through wall functionality

--- index.js
+++ index.js
@@ -77,6 +77,17 @@ function loop() {
   snake.x += snake.dx;
   snake.y += snake.dy;
 
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  } else if (snake.x >= canvas.width) {
+    snake.x = 0;
+  }
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  } else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+
   snake.cells.unshift({ x: snake.x, y: snake.y });
   if (snake.cells.length > snake.maxCells) {
     snake.cells.pop();
